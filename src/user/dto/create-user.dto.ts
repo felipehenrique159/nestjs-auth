4a1d16d761aa4da47b2ad7b565b3e2cd71493771
felipehenrique159 <felipecod6@hotmail.com>
2022-04-10 20:23:16 -0300

    Create de Usuario

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator"
+import { User } from "../entities/user.entity"
+
+export class CreateUserDto extends User {
+    @IsEmail()
+    email:string
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    password:string
+}
