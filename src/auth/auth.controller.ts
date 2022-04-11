11987929db7e573e253e9c73f7717173b0348867
felipehenrique159 <felipecod6@hotmail.com>
2022-04-11 14:49:11 -0300

    implementação dos guards e strategy

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, HttpCode, HttpStatus, Post,Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller()
+export class AuthController {
+
+    constructor(private readonly authService:AuthService){}
+    
+    //@IsPublic()
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    async login() {
+        
+    }
+    
+
+
+}
