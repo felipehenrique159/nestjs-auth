c48614cadc26312b0995a4fc2e8e4f12a13ff46d
felipehenrique159 <felipecod6@hotmail.com>
2022-04-12 21:06:10 -0300

    Protegendo as rotas da aplicação com o jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpCode, HttpStatus, Post,Req,Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
 
@@ -8,7 +9,7 @@ export class AuthController {
 
     constructor(private readonly authService:AuthService){}
     
-    //@IsPublic()
+    @IsPublic()
     @UseGuards(LocalAuthGuard)
     @Post('login')
     @HttpCode(HttpStatus.OK)
